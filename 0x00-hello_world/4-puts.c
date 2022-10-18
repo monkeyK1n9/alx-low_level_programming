39ae1fa4af2d5c6a4cdadfb04c39f94a227943af
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2022-10-17 19:37:03 -0700

    My first c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,6 @@
 /*Program to print a line*/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle")
-	return (0)
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
