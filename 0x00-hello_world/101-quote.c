d7aa53d93389d47a9deb8ce5be14848ed46da704
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2022-10-17 20:43:06 -0700

    last quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * prints quote
+ *
+ * Return: Always 1 (error)
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
