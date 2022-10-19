5c6cd14d19e8d0630fec515a383e63c98eb1db0b
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2022-10-18 19:29:54 -0700

    print alhabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,24 @@
-#include <stdio.h>
+#include <unistd.h>
 
-int printWord(char* word)
+/**
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+int _putchar(char c)
 {
-	printf("%s", word);
-	return (0);
+	return (write(1, &c, 1));
 }
+
 
 void print_alphabet(void)
 {
 	char i;
 
 	for (i = 'a'; i <= 'z'; ++i)
-		printf("%c", i);
-	printf("\n");
+		_putchar(i);
+	_putchar('\n');
 	return;
 }
