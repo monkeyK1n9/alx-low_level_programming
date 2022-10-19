e421644459ed985b7b066a9048b170936d939942
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2022-10-18 18:25:25 -0700

    print word

---
+++ 0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * fuction to print word
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return 0;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# alx-low_level_programming
+0-putchar.c is a C file that prints the word _putchar
