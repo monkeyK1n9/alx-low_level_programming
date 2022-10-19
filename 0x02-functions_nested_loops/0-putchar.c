fef634438d1fc66b9f8ead705181467fc639bec7
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2022-10-19 06:12:13 -0700

    Hot fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,11 +10,20 @@
 
 int main(void)
 {
-	char i[8] = "_putchar";
+	/*char i[8] = "_putchar";
 	int j;
 
 	for (j = 0; j <= 7; ++j)
-		_putchar(i[j]);
+		_putchar(i[j]);*/
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
