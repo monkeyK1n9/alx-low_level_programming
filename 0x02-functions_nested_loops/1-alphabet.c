300568ddc102227896fe9cfb4420d3b4f64543c5
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2022-10-19 20:02:34 -0700

    new Fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <unistd.h>
+
 /**
  * main - Entry point
  *
@@ -7,10 +7,6 @@
  *
  * Return: Always 0
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 
 void print_alphabet(void)
 {
