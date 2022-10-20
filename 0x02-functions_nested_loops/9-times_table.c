04518bf9c908d4a5de5ba01e9d1976bd87b88143
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2022-10-20 12:03:59 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * times_table - prints the times table
+ *
+ * Return: prints the time table
+ */
+
+void times_table(void)
+{
+	int i, j;
+
+	for (1 = 0; i <= 9; ++i)
+	{
+		for (j = 0; j <= 9; ++j)
+		{
+			_putchar((i*j) + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
