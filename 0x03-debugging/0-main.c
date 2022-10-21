16b802e1199b9b28661ebdcf8597039edd9f878b
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2022-10-20 17:33:56 -0700

    test sign of 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,16 +2,12 @@
 
 /**
  * main - Entry point
- *
- * checks if 0 verifys with positive_or_negative function
- *
  * Return: return 1 or 0 depending on result
  */
 
 int main(void)
 {
 	int i;
-	char t;
 
 	i = 0;
 	positive_or_negative(i);
