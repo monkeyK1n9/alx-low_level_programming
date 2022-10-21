9e041a672502359f66cb728eadda63a8e1403303
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2022-10-21 05:38:18 -0700

    print remaining days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -16,6 +16,11 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (!(year % 100 == 0))
 		{
+			if (month > 2)
+			{
+				++day;
+			}
+
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 366 - day);
 		}
@@ -34,7 +39,7 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		if (month == 2 && day == 60 && !(year % 4 == 0))
+		if (month == 2 && day == 60)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
