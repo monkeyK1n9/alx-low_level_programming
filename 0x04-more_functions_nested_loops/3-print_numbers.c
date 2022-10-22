b6e78f347a216e2ec492db06bcdf707c5282a0fe
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2022-10-21 19:02:13 -0700

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 to 9
+ *
+ * Return: list of numbers
+ */
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = 0; c <= 9; ++c)
+		_putchar(c + '0');
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 0-isupper.c checks if a character is uppercase
 1-isdigit.c checks if entry is a digit
 2-mul.c multiplies two integers
+3-print_numbers.c prints numbers from 0 to 9
