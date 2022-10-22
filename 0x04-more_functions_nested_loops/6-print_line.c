d98efb701fd68ce540e4b93271d198a59ea58e5a
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2022-10-21 19:59:28 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,7 +8,7 @@
 
 void print_line(int n)
 {
-	char i;
+	int i;
 
 	if (n <= 0);
 	{
@@ -16,8 +16,10 @@ void print_line(int n)
 	}
 	else
 	{
-		for (i = 0; i < n; ++i)
+		for (i = 1; i <= n; ++i)
+		{
 			_putchar('_');
+		}
 		_putchar('\n');
 	}
 }
