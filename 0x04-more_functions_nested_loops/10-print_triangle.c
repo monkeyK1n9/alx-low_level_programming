7787ae63e9744fdd2f8600a43939c191c90debf7
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2022-10-21 21:33:22 -0700

    Print tables

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -18,16 +18,15 @@ void print_triangle(int size)
 	{
 		for (i = 1; i <= size; ++i)
 		{
-			for (j = 1; j < (i + 2) / 2; ++j)
+			for (j = 1; j <= size - i; ++j)
 			{
 				_putchar(' ');
 			}
-			for (k = 1; k < (i + 2) / 2; ++k)
+			for (k = 1; k <= i; ++k)
 			{
 				_putchar('#');
 			}
 			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
