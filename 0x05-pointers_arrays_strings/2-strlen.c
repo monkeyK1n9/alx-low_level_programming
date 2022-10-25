a87bc0e98adca888cf15c03dd4a7ba51c2b1fc83
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2022-10-24 17:58:07 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strlen - computes the length of a string
+ *
+ * @s: string
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+	while (*s[i] != '\0')
+	{
+		++j;
+		++i;
+	}
+
+	return (j);
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,2 +1,3 @@
 0-reset_to_98 resets a value with pointers
 1-swap swaps the value of two integers
+2-strlen computes the length of a string
