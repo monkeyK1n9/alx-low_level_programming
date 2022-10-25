8cca513503f2265286e7e651b012b516fcc00b5a
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2022-10-24 20:43:18 -0700

    print n-number of elements

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,12 +12,12 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i <= n; ++i)
+	for (i = 0; i < n; ++i)
 	{
-		if (i < n)
+		if (i < n - 2)
 			printf("%d, ", a[i]);
 		else
-			printf("%d", a[n]);
+			printf("%d", a[i]);
 	}
 	printf("\n");
 }	
