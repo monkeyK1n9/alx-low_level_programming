07cd4854c83fb73361fa4ecce42c3a7e2e1b051a
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2022-10-26 14:00:37 -0700

    concat n-characters to string

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenate n characters of a string to another
+ * @dest: first string to be added
+ * @src: second string to take n-character from
+ * @n: number of characters
+ * Return: returns the pointer to dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	while (!(*(dest + i) == 0))
+	{
+		++i;
+	}
+
+	for (j = i; j <= i + n; ++j)
+	{
+		*(dest + j) = *(src + j - i);
+	}
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,1 +1,2 @@
 0-strcat.c concatenates two strings
+1-strncat concatenate towo strings with n-characters from the second string
