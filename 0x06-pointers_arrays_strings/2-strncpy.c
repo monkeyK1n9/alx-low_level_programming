604a8eef7e333afb8f67ec58e8846772a3326f30
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2022-10-27 04:11:10 -0700

    Copy n character string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,13 +13,12 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	i = 0;
-	for (i = 0; i <= n - 1; i++)
+	for (i = 0; !(*(src + i) == '\0'); i++)
 	{
-		*(dest + i) = *(src + i);
+		if (i < n)
+			*(dest + i) = *(src + i);
 		while (i < n)
 			*(dest + i + 1) = '\0';
-		if (*(src + i) == '\0')
-			i = n;
 	}
 	return (dest);
 }
