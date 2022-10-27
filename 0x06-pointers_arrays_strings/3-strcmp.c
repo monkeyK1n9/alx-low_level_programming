524358387c482e8a61b7f307e4789318cfdea931
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2022-10-27 06:59:06 -0700

    compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,20 +9,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, m;
+	int i;
 
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
 		{
-			m = s1[i] - s2[i];
-			break;
-		}
-		else
-		{
-			m = 0;
-			break;
+			return (s1[i] - s2[i]);
 		}
 	}
-	return (m);
+	return (0);
 }
