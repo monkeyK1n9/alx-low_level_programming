2c613dfd931416674e94ac2212e89ad6432338bd
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2022-10-27 08:36:05 -0700

    reverse integers

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -21,8 +21,8 @@ void reverse_array(int *a, int n)
 		{
 			n--;
 			tmp = a[i];
-			*(a + i) = *(a + n - 1 - i);
-			*(a + n - 1 - i) = tmp;
+			*(a + i) = *(a + n);
+			*(a + n) = tmp;
 		}
 	}
 }
