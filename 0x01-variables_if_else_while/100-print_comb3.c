c498b625f9d07e98b2f2401f21a4e5740a25bc8b
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2022-10-27 18:04:17 -0700

    print combination of numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,15 +10,17 @@
 
 int main(void)
 {
-	int d;
+	int digit1, digit2;
 
-	for (d = 0; d < 90; d++)
+	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-		putchar((d / 10) + '0');
-		putchar((d % 10) + '0');
-
-		if (d != 89)
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			if (digit1 == 8 && digit2 == 9)
+				continue;
 			putchar(',');
 			putchar(' ');
 		}
