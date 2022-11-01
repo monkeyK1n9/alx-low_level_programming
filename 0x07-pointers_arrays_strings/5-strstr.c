9ca7a8283ea72f1a84aef7cfd7469b3697c3ea61
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2022-11-01 16:16:54 -0700

    Hot fix

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,6 +13,9 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j, k, m;
 
+	if (*needle == 0)
+		return (haystack);
+
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
