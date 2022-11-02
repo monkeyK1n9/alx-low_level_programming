54f1791e692261064f0e00dbbc50485d7cc703c9
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2022-11-02 16:56:04 -0700

    Put recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,12 +8,9 @@
 
 void _puts_recursion(char *s)
 {
-	unsigned int i;
-
-	i = 0;
 	if (*s == '\0')
 		_putchar('\n');
 
 	_putchar(*s);
-	_puts_recursion(&(s[i++]));
+	_puts_recursion(&(*(s + 1)));
 }
