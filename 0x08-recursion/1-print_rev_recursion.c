851bb35f20398abb13f7e5af9bc7f9388b7230c6
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2022-11-02 18:28:31 -0700

    Reverse string

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,20 +8,14 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*(s - 1))
+	if (*(s) != '\0')
 	{
-		_putchar(*(s - 1));
-		_print_rev_recursion(&(*(s - 2)));
-		_putchar('\n');
-		return;
+		_print_rev_recursion(&(*(s + 1)));
 	}
-	if (*s == '\0')
+	else
 	{
-		_putchar(*(s - 1));
-		_print_rev_recursion(&(*(s - 1)));
-		_putchar('\n');
 		return;
 	}
 
-	_print_rev_recursion(&(*(s + 1)));
+	_putchar(*s);
 }
