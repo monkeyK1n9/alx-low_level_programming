7c60fc93ed41dc0ea54c8af7656035c593d1a60b
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2022-11-02 19:34:15 -0700

    count string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,12 +8,8 @@
 
 int _strlen_recursion(char *s)
 {
-	int i;
-
-	i = 0;
 	if (*s != '\0')
-		_strlen_recursion(s + 1);
+		return (0);
 	else
-		return (i);
-	i++;
+		return (1 + _strlen_recursion(s + 1));
 }
