00e44b02b5f11675d9e1f604c479a236d04a7666
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2022-11-08 15:23:41 -0800

    add numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: number of entries
+ * @argv: array of entries
+ * Return: Always success 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum;
+
+	sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; argv[i] != '\0'; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (isdigit(argv[i][j]))
+			{
+				continue;
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -2,3 +2,4 @@
 1-args.c prints the number of arguments printed into it
 2-args.c prints all arguments
 3-mul.c command function that multiply two functions
+4-add.c adds multiple numbers
