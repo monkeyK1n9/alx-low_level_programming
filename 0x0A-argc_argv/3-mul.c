43a1b90337eac4427bf871e812614c864e3c4df6
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2022-11-08 13:59:59 -0800

    multiply 2 nums

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: number of entries
+ * @argv: array of entries
+ * Return: Always success 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j;
+
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+
+	printf("%d\n", i * j);
+
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,3 +1,4 @@
 0-whatsmyname.c prints the program name
 1-args.c prints the number of arguments printed into it
 2-args.c prints all arguments
+3-mul.c command function that multiply two functions
