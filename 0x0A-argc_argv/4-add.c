e867f8177fbabe35aa80a20cbbfbdc0d72c13a37
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2022-11-08 17:32:20 -0800

    hot fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,7 +11,7 @@
 
 int main(int argc, char *argv[])
 {
-	int i, sum;
+	int i, j, sum;
 
 	sum = 0;
 
@@ -22,10 +22,13 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; ++i)
 	{
-		if (!atoi(argv[i]))
+		for (j = 0; argv[i][j]; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
 
 		sum += atoi(argv[i]);
