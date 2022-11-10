cd0ec90a5590268f11fcafa041df998945bbba63
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2022-11-09 17:54:42 -0800

    concat strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: pointer to the newly allocated space
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i, j, k;
+	char *str;
+
+	i = 0;
+	j = 0;
+	while (s1[i])
+		++i;
+	while (s2[j])
+		++j;
+
+	arr = malloc((sizeof(char) * i) + (sizeof(char) * j) + 1);
+	if (arr == NULL)
+		return (NULL);
+
+	for (k = 0; k < i; k++)
+		arr[k] = s1[k];
+	for (k = i; k < i + j; k++)
+		arr[k] = s2[k - i];
+	arr[i + j] = '\0';
+
+	return (arr);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,2 +1,3 @@
 0-create_array.c creates an array of chars and initializes it with a specific char
 1-strdup.c returns a pointer to a newly allocated space in memory which contains copy of the string
+2-str_concat.c concatenates two strings
