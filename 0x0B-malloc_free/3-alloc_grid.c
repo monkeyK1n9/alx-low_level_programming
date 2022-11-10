2bdf78e8f1c9f9151ea3165c8373d64128932bd0
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2022-11-09 18:12:24 -0800

    2D array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - gives pointer to 2D array of integers
+ * @width: width of the array
+ * @height: height of the array
+ * Return: pointer to 2D array
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int *arr;
+
+	if (height <= 0 || width <= 0)
+		return (NULL);
+	arr = malloc(sizeof(int) * width * height);
+
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < width; ++i)
+	{
+		for (j = 0; j < height; ++j)
+		{
+			arr[i][j] = 0;
+		}
+	}
+
+	return (arr);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,3 +1,4 @@
 0-create_array.c creates an array of chars and initializes it with a specific char
 1-strdup.c returns a pointer to a newly allocated space in memory which contains copy of the string
 2-str_concat.c concatenates two strings
+3-alloc_grid.c returns a pointer to a 2D arrays of integers
