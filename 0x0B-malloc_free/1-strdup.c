3da14aeb7f9d1b7504f9fa36c1e9e0a69f70d4a7
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2022-11-09 16:43:23 -0800

    copy of string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - returns pointer of space which contains
+ * a copy of the string
+ * @str: string to copy in allocated space
+ * Return: pointer to allocated space
+ */
+
+char *_strdup(char *str)
+{
+	char *arr;
+	int i;
+
+	i = 0;
+	while (str[i])
+		++i;
+
+	arr = malloc(sizeof(char) * i);
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		arr[i] = str[i];
+
+	return (arr);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,1 +1,2 @@
 0-create_array.c creates an array of chars and initializes it with a specific char
+1-strdup.c returns a pointer to a newly allocated space in memory which contains copy of the string
