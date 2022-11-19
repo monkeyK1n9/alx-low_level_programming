355501e09edf92a0f717f10a5f345aa43bd6f60a
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2022-11-19 01:49:28 -0800

    Array alloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates space to an array of specific size
+ * @nmemb: number of elements in array
+ * @size: size of array to allocate
+ * Return: nothing
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *arr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	arr = malloc(nmemb * size);
+
+	if (arr == NULL)
+		return (NULL);
+	return (arr);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -1,2 +1,3 @@
 0-malloc_checked.c allocates a memory space
 1-strin_nconcat.c concatenates two strings
+2-calloc.c allocates memory for an array
