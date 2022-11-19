b26f02003a893cc33588739fb30b072daf04811a
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2022-11-19 02:00:33 -0800

    hot fix

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,6 +4,7 @@
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void simple_print_buffer(char *buffer, unsigned int size);
 void *_calloc(unsigned int nmemb, unsigned int size);
 
 #endif
