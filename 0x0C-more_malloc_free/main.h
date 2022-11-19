553ad36909e3d6fb599e8a296f77c3823d26f229
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2022-11-19 04:31:57 -0800

    creates array of integers

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,6 +6,6 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void simple_print_buffer(char *buffer, unsigned int size);
 void *_calloc(unsigned int nmemb, unsigned int size);
-void *array_range(int min, int max);
+int *array_range(int min, int max);
 
 #endif
