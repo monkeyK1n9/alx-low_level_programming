e76cda312507894d58cb54c29efc1e80b3c83991
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2022-11-23 03:46:20 -0800

    free dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -10,8 +10,17 @@
 
 void free_dog(dog_t *d)
 {
-	free(d->name);
-	free(d->age);
-	free(d->owner);
+	if (d == NULL)
+		return (NULL);
+
+	if (d->name != NULL)
+		free(d->name);
+
+	if (d->age > 0)
+		free(d->age);
+
+	if (d->owner != NULL)
+		free(d->owner);
+
 	free(d);
 }
