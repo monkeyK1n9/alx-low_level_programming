37384b9032b09ffced659ab3ca7157b02b51d766
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2023-01-12 13:57:16 -0800

    size fix

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,4 @@
-#include "function_pointers.h"
+i#include "function_pointers.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -12,7 +12,7 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	unsigned int i;
 
 	if (array == NULL)
 		return;
@@ -24,6 +24,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	if (size == NULL || size != i || action == NULL)
 		return;
 
-	for (i = 0; array[i] != '\0'; i++)
+	for (i = 0; i < size; i++)
 		action(array[i]);
 }
