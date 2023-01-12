cbbaf72ddcb04f1b2f3dd98eef3c2276ef608bb5
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2023-01-12 14:55:55 -0800

    hot fix

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -17,7 +17,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	if (array == NULL)
 		return;
 
-	if (size == NULL || action == NULL)
+	if (action == NULL)
 		return;
 
 	for (i = 0; i < size; i++)
