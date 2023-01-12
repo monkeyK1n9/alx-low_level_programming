9d9844d6af78714ba8fcb3fee58651fce8f429bc
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2023-01-12 12:55:57 -0800

    hot fix

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,8 +2,6 @@
 #define FUNCTION_POINTERS_H
 
 int _putchar(char c);
-void print_name_as_is(char *name);
-void print_name_uppercase(char *name);
 void print_name(char *name, void (*f)(char *));
 
 #endif
