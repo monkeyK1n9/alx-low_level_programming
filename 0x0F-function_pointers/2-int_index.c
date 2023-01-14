82e58cfd63d886a16c07eefdfef687f33eeda145
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2023-01-13 16:37:24 -0800

    hot fix

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,15 +8,15 @@
  * @size: size of the array
  * @cmp: compare function to look for array
  * Return: returns index of element
- * 	if no element matches, return -1,
- * 	if size <= 0, return -1
+ * if no element matches, return -1,
+ * if size <= 0, return -1
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (array == NULL || cmp == NULL || size <= 0)
 		return (-1);
 
 	for (i = 0; i < size; i++)
@@ -24,7 +24,6 @@ int int_index(int *array, int size, int (*cmp)(int))
 		if (cmp(array[i]) == 0)
 		{
 			return (i);
-			break;
 		}
 	}
 
