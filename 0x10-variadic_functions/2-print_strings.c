5751ae7f6b8af21e018f465b35f12bbf4ccdf3ef
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2023-01-18 15:16:38 -0800

    print strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - prints the strings given as arguments
+ * @separator: the separator between strings
+ * @n: number of strings to print
+ * @...: the other arguments
+ * Return: nothing
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	int i;
+	va_list args;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		if (va_arg(args, int) != NULL)
+			printf("%s", va_arg(args, int));
+		else
+			printf("(nil)");
+
+		if (separator != NULL && i != n - 1)
+			printf("%s", separator);
+	}
+
+	va_end(args);
+	putchar('\n');
+}
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -1,2 +1,3 @@
 sum_them_all sums all of its parameter
 1-print_numbers prints integer entries
+2-print_strings prints the strinfs entered as arguments
