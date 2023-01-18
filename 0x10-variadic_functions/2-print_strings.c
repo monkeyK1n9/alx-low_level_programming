3b5bd805df234e603502d5edadfbef94531566e0
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2023-01-18 15:27:54 -0800

    fix typo

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		if (va_arg(args, int) != NULL)
+		if (va_arg(args, char *) != NULL)
 			printf("%s", va_arg(args, char *));
 		else
 			printf("(nil)");
