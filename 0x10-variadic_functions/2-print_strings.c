4cd70f27e4a9a0bf96820f9d264fda9dad43324c
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2023-01-18 15:31:06 -0800

    fix typo

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,10 +24,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		else
 			printf("(nil)");
 
-		if (separator != NULL && i != n - 1)
+		if (separator != NULL && i != (n - 1))
 			printf("%s", separator);
 	}
 
 	va_end(args);
-	putchar('\n');
+	printf("\n");
 }
