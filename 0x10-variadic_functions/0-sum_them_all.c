696b2a0fa084d50c4efdfe59bb9ed66399f9964c
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2023-01-18 11:42:45 -0800

    first variadic

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - returns sum of all its parameters
+ * @n: first parameter
+ * Return: always 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	int sum = 0;
+
+	if (n == 0)
+		return (0);
+	va_start(args, n);
+	for (i = n; i >= 0; i = va_arg(args, int))
+		sum += va_arg(args, int);
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+sum_them_all sums all of its parameter
