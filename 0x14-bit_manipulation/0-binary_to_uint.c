71c296e6845b1fb4a8f948201962a3c8aea8cc07
Guillaume DJIODJIO <mnkeyk1n9@gmail.com>
2023-01-25 03:51:02 -0800

    binary to int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -20,7 +20,7 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL || b == '\0')
 		return (0);
 
-	while (b[i])
+	while (b[i] != '\0')
 	{
 		if (b[i] != '0' || b[i] != '1')
 			return (0);
